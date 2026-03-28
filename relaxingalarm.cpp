d5af9d44fbabb4dd240c4bc027219026ab6d645b
Sajeda Hmeda <sajedahm18@icloud.com>
2026-03-29 00:25:58 +0200

    Add files via upload
    add user input, state management , and improved console interface.

--- relaxingalarm.cpp
+++ relaxingalarm.cpp
@@ -1,21 +1,35 @@
 #include<iostream>
 #include<windows.h>
 using namespace std;
-int main(){
+int main()
+{
     cout<<"Relaxing Alarm started"<<endl;
     int relaxinterval;
     int time;
-    cout<<"How mutch time you want to work!"<<endl;
+    char answer;
+    while(true)
+    {
+        system("cls");// clear the console screen
+    cout<<"How much time you want to work?"<<endl;
     cin>>time;
     relaxinterval=time*60;
-    while(true){
+    cout<<"Relaxing Alarm set for "<<time<<" minutes.press s to start"<<endl;
+    cin>>answer;
+    if(answer=='s' || answer=='S')
+    {
         cout<<"time started"<<endl;
         Sleep(relaxinterval*1000);
         cout<<"Time to relax your eyes!"<<endl;
         Beep(750,1000); // beep sound for eye relaxation reminder
         MessageBox(NULL,"Time to relax your eyes!","Eye Relaxation Reminder",MB_OK|MB_ICONEXCLAMATION);
-        
-
     }
+        else if (answer=='q' || answer=='Q')
+        {
+            cout<<"Exiting the program. Take care of your eyes!"<<endl;
+            break;
+        }
+
+    
+        }
     return 0;
 }
